47c482efa29d275094f6a0dfed2b6a385f22cbf4
Killian Comerford <killiancomerford@outlook.com>
2020-02-29 11:28:51 +0100

    Working v1.0
    Still needs some changes, it tends to create long hallways when map size grows

---
+++ Game1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Map_Generator
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        KeyboardState lastKeyboardState;
+
+        GridGenerator generator;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            generator = new GridGenerator();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            generator.GenerateGrid();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            generator.LoadContent(this);
+        }       
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+
+            //&& lastKeyboardState.IsKeyUp(Keys.G)
+            if (keyboard.IsKeyDown(Keys.G) )
+                generator.GenerateGrid();
+
+            lastKeyboardState = keyboard;
+
+            base.Update(gameTime);            
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            generator.DrawGrid(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
